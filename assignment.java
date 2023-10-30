f2acfb4ba2a09bca7111a27c8fca653e18d7ba49
Enosh roy <enoshroy56@gmail.com>
2023-10-30 16:32:26 +0530

    first java array problem

---
+++ assignment.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+ class ShuffleArray {
+   public static void main(String[] args) {
+    
+    Integer[] array = {1, 2,3,4, 5, 6, 7};
+
+    List<Integer> list = new ArrayList<>(Arrays.asList(array));
+ 
+    Collections.shuffle(list);
+
+    System.out.println("Shuffled Array; "+list);
+  }
+}
