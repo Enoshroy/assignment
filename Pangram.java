6992877b5282c84013b1c5188d069eeefe9ae817
Enosh roy <enoshroy56@gmail.com>
2023-10-30 23:22:21 +0530

    Pangram

---
+++ Pangram.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Pangram{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        boolean continueChecking = true;
+        while (continueChecking) {
+           
+            System.out.println("Please enter a sentence: ");
+            String userInput = scanner.nextLine().toLowerCase(); 
+            
+            String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            boolean isPangram = true;
+
+            for (char letter : alphabet.toCharArray()) {
+                if (!userInput.contains(String.valueOf(letter))) {
+                    isPangram = false;
+                    break;
+                }
+            }
+            if (isPangram) {
+                System.out.println("The sentence is a pangram.");
+            } else {
+                System.out.println("The sentence is not a pangram.");
+                System.out.println("Would you like to check another sentence? (yes/no)");
+                String response = scanner.nextLine().toLowerCase();
+                if (!response.equals("yes")) {
+                    continueChecking = false;
+                }
+            }
+        }
+    }
+}
