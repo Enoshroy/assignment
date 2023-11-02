5ea5c471f1bb29cb54726d6e81ea55a3a7ecad56
Enosh roy <enoshroy56@gmail.com>
2023-11-02 12:33:14 +0530

    assignment done

--- JS(B Sorted array).js
+++ JS(B Sorted array).js
@@ -1,15 +1,20 @@
-function sortDescending(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] < arr[j]) {
-                const temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
+function sortArray() {
+    const inputArray = document.getElementById('inputArray').value;
+    const resultElement = document.getElementById('result');
+    const array = inputArray.split(',').map(Number); // Convert comma-separated input to an array
+
+    if (array.length > 1) {
+      array.sort(function(a, b) {
+        return b - a;
+      });
+
+      resultElement.textContent = 'Sorted Array (Descending): ' + array.join(', ');
+    } else {
+      resultElement.textContent = 'Please enter a valid array (at least two elements).';
     }
-    return arr;
-}
-const inputArray = [5, 1, 9, 3, 7];
-const sortedArray = sortDescending(inputArray);
-console.log(sortedArray);
+  }
+
+  function resetArray() {
+    document.getElementById('inputArray').value = '';
+    document.getElementById('result').textContent = '';
+  }
